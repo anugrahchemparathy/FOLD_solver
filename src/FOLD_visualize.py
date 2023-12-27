474a8428d42b72824f71b2f6aa759ef69c0d9f68
Anugrah Chemparathy <anugrahchemparathy@gmail.com>
2023-12-26 20:27:50 -0800

    clean up visualization code in src

--- src/FOLD_visualize.py
+++ src/FOLD_visualize.py
@@ -11,22 +11,27 @@ def run_visualization(folded_form, clear_vis = True):
     canvas.pack()
 
     def draw_polygon(canvas, points, outline_color = 'white', clear_vis = True):
-        scale = lambda x: 600 * (x) + 100
-        jitter = np.random.randn() * 4 # to help visualize distinct polygons if edges overlap
-        transform = lambda x: scale(x) + (jitter if not clear_vis else 0)
-        
+        scale = lambda x: 600 * x + 100
+
+        # OPTION 1: jitter entire polygon at once
+        jitter = lambda : np.random.randn() * 4 # to help visualize distinct polygons if edges overlap
+        transform = lambda x: scale(x) + (jitter() if not clear_vis else 0)
+
+        # OPTION 2: jitter points individually for every polygon
+        # jitter = np.random.randn() * 3 # to help visualize distinct polygons if edges overlap
+        # transform = lambda x: scale(x) + (jitter if not clear_vis else 0)
+
+
         new_points = []
         for px, py in points:
             new_points += [transform(px), transform(py)]
         
         canvas.create_polygon(new_points, outline=outline_color, fill='', width=0.1)
 
-    tkinter_colors = [
-        "white", "red", "green", "cyan", "yellow",
-        "orange", "purple", "pink",
-        "#FF0000", "#00FF00", "#0000FF", "#FFFF00", "#FF00FF", "#00FFFF", "#FFA07A"
-    ]
-    tkinter_colors = ['white']
+    if clear_vis:
+        tkinter_colors = ['white']
+    else:
+        tkinter_colors = ["white", "red", "green", "cyan", "yellow", "orange", "pink", "#F0E68C", "#7CFC00"]
 
     for polygon_points in folded_form:
         color = random.choice(tkinter_colors)
@@ -46,11 +51,14 @@ def get_form(filepath):
             polygon.append((vertex["x"], vertex["y"]))
         
         folded_form.append(polygon)
-    # print(folded_form)
     return folded_form
 
 if __name__ == "__main__":
     filepath = "out.json"
     folded_form = get_form(filepath)
+
+    # OPTION 1: visualize without jitter
     # run_visualization(folded_form, clear_vis=True)
+
+    # OPTION 2: visualize with jitter and randomly colored polygons
     run_visualization(folded_form, clear_vis=False)
